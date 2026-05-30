603e95fe2449ce823c72ff315901c77bb35e79d0
Masaki-JP <masakidayo0508@gmail.com>
2026-05-30 20:17:39 +0900

    初期コミット

---
+++ Practice-MultiModule/Practice_MultiModuleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Practice_MultiModuleApp.swift
+//  Practice-MultiModule
+//
+//  Created by Masaki Doi on 2026/05/30.
+//
+
+import SwiftUI
+
+@main
+struct Practice_MultiModuleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
