2a11bac60559788012e2192838ada16df7d91d8b
Masaki-JP <masakidayo0508@gmail.com>
2026-05-31 21:35:25 +0900

    PokemonPackageのライブラリ名をPokemonLibraryに変更

--- PokemonPackage/Package.swift
+++ PokemonPackage/Package.swift
@@ -9,7 +9,7 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "PokemonPackage",
+            name: "PokemonLibrary",
             targets: ["PokemonPackage"]
         ),
     ],
